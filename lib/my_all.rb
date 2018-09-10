3c74075a7efb39e4ba7110f953ebdfbb7ab1f119
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-10 20:20:39 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,12 +3,8 @@ require 'pry'
 def my_all?(collection)
   i = 0
   while i < collection.size
-    if yield(collection[i])
-      binding.pry
-      i += 1
-    else
-      return false
-    end
-    true
+    yield(collection[i])
+    binding.pry
+    i += 1
   end
 end
